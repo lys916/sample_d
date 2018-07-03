210ba73b3558acec7c8095a87647980257cad600
Cliff Kang <inlovewithhim@gmail.com>
2018-07-02 22:20:32 -0700

    got necessary venue info on redux store, including photo and ratings

--- frontend/src/actions/index.js
+++ frontend/src/actions/index.js
@@ -1,53 +1,42 @@
 import axios from 'axios';
 
+const clientId = 'F5RTJTTG122SE1BHB0GE1OP2PACQLGBNUFPFAKT4HB2CR0OX';
+const clientSecret = 'A0RLUDRE4GIA201TOUVE5KM0SYMTSPZD50XYOP1HNUX1DQV0';
 const searchURL = 'https://api.foursquare.com/v2/venues/search';
 const detailURL = 'https://api.foursquare.com/v2/venues';
-const clientId = 'EI3GHFP5FIFERWKVR2SYXSTOO4BKYZV33CLRVCHSCCKZJ0DF';
-const clientSecret = 'MG3Z0KQILIDDRIOCIWFMUDVF4QWL5C5RTVZDYS0SON5ZLAHF';
 
 export const getLocation = (data)=>{
 	console.log('loca', data);
-	if(data.location){
-		// search venues by city
-		const qs = { 
-			client_id: clientId, 
-			client_secret: clientSecret,
-			near: data.location,
-			query: data.query,
-			v: '20180702',
-			venuePhotos: 1
-		}
-		return (dispatch) => {
-			console.log('request axio');
-			axios.get(`${searchURL}`, {params: qs}).then(res => {
-				console.log(res.data.response.venues);
-				dispatch({
-					type: 'FETCHED_VENUES',
-					payload: res.data.response.venues
-				});
+	let qs = {
+		client_id: clientId, 
+		client_secret: clientSecret,
+		query: data.query,
+		limit: 5,
+		v: '20180702',
+	};
+	if (data.location) qs.near = data.location;
+	else qs.ll = `${data.lat},${data.long}`;
+	return (dispatch) => {
+		console.log('request axio');
+		axios.get(`${searchURL}`, {params: qs}).then(res => {
+			const { venues } = res.data.response;
+			venues.forEach(venue => {
+				axios.get(`${detailURL}/${venue.id}`, { params: qs })
+					.then(res => {
+						venue.bestPhotoPrefix = res.data.response.venue.bestPhoto.prefix;
+						venue.bestPhotoSuffix = res.data.response.venue.bestPhoto.suffix;
+						venue.rating = res.data.response.venue.rating;
+						venue.numOfRatings = res.data.response.venue.ratingSignals;
+						console.log('venue details result is', res.data.response.venue)
+					})
+			})
+			console.log(res.data.response.venues);
+			dispatch({
+				type: 'FETCHED_VENUES',
+				payload: venues
 			});
-    	}
-	}else{
-		// serach venues by lat and long
-		const qs = { 
-			client_id: clientId, 
-			client_secret: clientSecret,
-			ll: `${data.lat},${data.long}`,
-			query: data.query,
-			v: '20180702',
-			venuePhotos: 1
-		}
-		console.log('qs', qs);
-		return (dispatch) => {
-	      axios.get(`${searchURL}`, {params: qs}).then(res => {
-	      	console.log(res.data.response.venues);
-	       	dispatch({
-	       		type: 'FETCHED_VENUES',
-	       		payload: res.data.response.venues
-	       	});
-	      });
-    	}
-	}	
+		});
+	}
 }
 
 export const getVenueDetail = (id)=>{
