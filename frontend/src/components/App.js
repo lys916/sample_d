cc19e1571abe77a02f35dbecc8a11f41160e95f4
Cliff Kang <inlovewithhim@gmail.com>
2018-07-05 13:03:33 -0700

    added some routes

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Home from './Home';
-import Venue from './Venue';
-// import Rate from './Rate';
+import Camera from './Camera';
+import ItemInput from './ItemInput';
 
 class App extends Component {
   render() {
@@ -11,8 +11,9 @@ class App extends Component {
       <div className="App">
         <Router>
           <Switch>
-            <Route path='/venues/:id' component={Venue} />
-            <Route path='/' component={Home}  />
+            <Route path='/takePhoto' component={Camera} />
+            <Route path='/addItem' component={ItemInput} />
+            <Route path='/' exact component={Home}  />
           </Switch>
         </Router>
       </div>
