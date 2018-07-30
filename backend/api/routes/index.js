331eb92f5ff1e10fea399ea3af10d73b92ce422d
Cliff Kang <inlovewithhim@gmail.com>
2018-07-30 10:47:38 -0700

    fixed merge conflict in server routes

--- backend/api/routes/index.js
+++ backend/api/routes/index.js
@@ -14,11 +14,7 @@ module.exports = server => {
     server.route('/addRating').post(addRating);
     server.route('/nearbyItems').get(nearbyItems);
     server.route('/searchItems').get(searchItems);
-<<<<<<< HEAD
     server.route('/fetchMenu').get(fetchMenu);
-=======
     server.route('/item').get(item);
-    server.route('/menu').get(menu);
->>>>>>> 57be68a0e6258b1431d9ca6f9aba1be8826fab09
     // server.route('/items').get(findItems);
 }
